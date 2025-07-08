e5e97e363c7e8481f8d9cda66243dc5d4cb5cc55
treasuremargie <treasuremargie@gmail>
2025-07-08 17:37:03 +0300

    write

---
+++ js.js
@@ -0,0 +1,9 @@
+//initial list of quotes
+let quotes=[
+    {text:"Believe you can you're halfway there.",author:"Theodore Roosevelt"},
+    {text:"You are stronger than you think.",author:"Unknown"},
+    {text:"Every day is a second chance.",author:"Oprah Weinfrey"},
+    {text:"Dream big.Start small.Act now.",author:"Robin Sharma"},
+    {text:"The best way out is always through.",author:"Robert Frost"}
+];
+//Load quotes from localStorage if available if (localStorage.getltem)
